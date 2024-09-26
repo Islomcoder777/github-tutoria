b29ef08653e2a06e815cae4f3a4fac70a65a1c34
Islom <example@gmail.com>
2024-09-26 23:28:05 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,56 @@
+
+// let incr = 10;
+// let decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(incr++);
+// console.log(decr--);
+
+
+// console.log(incr);
+// console.log(decr);
+
+// pratsent
+// console.log(10 % 3);
+
+// tenlashtirish
+// console.log(5 * 5 == 25);
+
+// tenlashtirish malumot turi bln
+// console.log(5 * 5 === "25");
+
+// && = va || = yoki
+
+// vaqat true qiymati bolishi kerek &&
+// const isAge = true;
+// const isclose = true;
+// const isCheket = true;
+
+// console.log(isAge && isclose && isCheket);
+
+// bitta true bolsaham true qiymatini qaytaradi
+// const isAge = true;
+// const isclose = false;
+// const isCheket = false;
+
+// console.log(isAge || isclose || isCheket);
+
+// ! true ni false ga false ni true ga aylantiradi
+// const cet = false;
+// console.log(!cet);
+
+// console.log( 2 * 2 + 2 === 6);
